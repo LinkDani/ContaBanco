1f6537da3c5bd89fe6727c1dca02747eb3bdaba7
Daniel Walter <danielwalterpand@gmail.com>
2017-09-05 20:55:53 -0300

    ImprimeDados Complete

--- src/br/com/bancoprogresso/Main.java
+++ src/br/com/bancoprogresso/Main.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
 		System.out.println("Cadastro de conta");
-		Sistema candidato = new Sistema("Daniel", "Instituto");
+		
+		System.out.println("Digite o nome: ");
+		String Nome = s.next();
+		
+		System.out.println("Digite o nome da Agência: ");
+		String Agencia = s.next();
+		
+		System.out.println("Digite seu número: ");
+		String Numero = s.next();
+		
+	
+		Sistema candidato = new Sistema(Nome, Agencia, Numero);
 		candidato.ImprimeDados();
 	}
 
